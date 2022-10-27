51261b22da1ce6c43be79758ddc1215c37d96f41
Jishna-tp <84098652+Jishna-tp@users.noreply.github.com>
2022-10-27 11:18:23 +0530

    Add files via upload

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Sidebar from '../Sidebar/Sidebar';
 import * as IoIcons from 'react-icons/io';
 import * as CgIcons from 'react-icons/cg';
 import './Header.css'
+import ProModal from '../Modal/ProModal';
 
 const Header = () => {
+  const [openModal, setOpenModel] = useState(false);
+
   return (
     <div className='header'>
+      <div className='header_items'>
         <Sidebar />
             <div className='search'>
                 <input type="search" className='search_bar' name="" id="" />
@@ -14,8 +18,11 @@ const Header = () => {
             </div>
             <div className='icons'>
                 <IoIcons.IoIosNotifications className='icon1' />
-                <CgIcons.CgProfile className='icon2' />
+                <CgIcons.CgProfile className='icon2' onClick={() => {setOpenModel(true);}} />
             </div>   
+      </div>
+      {openModal && <ProModal closeModal={setOpenModel} />}
+
     </div>
   )
 }
